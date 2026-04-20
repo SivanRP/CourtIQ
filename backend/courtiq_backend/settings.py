295481df66789afce21cd5b0fd87bae6d0deacf2
Sivan Reddy Pushpagiri <sivanrp515@gmail.com>
2026-04-20 16:50:17 -0400

    Add SQLite DATABASES config required by Django 5.2

--- backend/courtiq_backend/settings.py
+++ backend/courtiq_backend/settings.py
@@ -71,13 +71,12 @@ WSGI_APPLICATION = 'courtiq_backend.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=config("DATABASE_URL")
-#     )
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
